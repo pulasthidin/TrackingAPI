795d8464a61a87b0cbed38f8e49a509d33ca506a
PulasthiRanathunga <PulasthiRanathunga@CML-PULASTHIRAN>
2022-08-13 22:54:57 +0530

    Add project files.

---
+++ TrackingAPI/Controllers/IssueController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TrackingAPI.Data;
+using TrackingAPI.Models;
+
+namespace TrackingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get() => await _context.Issues.ToArrayAsync();
+
+        [HttpGet("id")] //api/issue/5
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);
+        }
+
+        [HttpPut("id")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Uodate(int Id, Issue issue)
+        {
+            if (Id != issue.Id) return BadRequest();
+
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
+
+        [HttpDelete("id")] 
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var issueToDelete = await _context.Issues.FindAsync(id);
+            if (issueToDelete == null) NotFound();
+
+            _context.Issues.Remove(issueToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
